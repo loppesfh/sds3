66068ca682afc16568600b818ed168d4f2a8a2a0
Loppes Filho <loppes@megganet.com.br>
2021-05-14 17:42:30 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/sds3/Sds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
